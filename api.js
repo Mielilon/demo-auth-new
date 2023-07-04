e27531a664d9a07e78c353f5efcd7476fb0d899e
Daria Magomedova <mielilon@gmail.com>
2023-07-04 23:56:15 +0300

    Добавлен токен

--- api.js
+++ api.js
@@ -1,22 +1,36 @@
+const host = "https://wedev-api.sky.pro/api/v2/todos";
+
+const token =
+  "Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k";
+
 export function getTodos() {
-  return fetch("https://webdev-hw-api.vercel.app/api/todos", {
+  return fetch(host, {
     method: "GET",
+    headers: {
+      Authorization: token,
+    },
   }).then((response) => {
     return response.json();
   });
 }
 
 export function deleteTodo({ id }) {
-  return fetch("https://webdev-hw-api.vercel.app/api/todos/" + id, {
+  return fetch(`${host}/${id}`, {
     method: "DELETE",
+    headers: {
+      Authorization: token,
+    },
   }).then((response) => {
     return response.json();
   });
 }
 
 export function postTodo({ text }) {
-  return fetch("https://webdev-hw-api.vercel.app/api/todos", {
+  return fetch(host, {
     method: "POST",
+    headers: {
+      Authorization: token,
+    },
     body: JSON.stringify({
       text,
     }),
